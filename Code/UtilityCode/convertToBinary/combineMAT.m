c8e89468d440927232f99499b9f31ff89513dbf9
syedshabihhasan <hasanshabih@gmail.com>
2014-12-02 09:29:03 -0600

    display where the loop is at

--- Code/UtilityCode/convertToBinary/combineMAT.m
+++ Code/UtilityCode/convertToBinary/combineMAT.m
@@ -5,6 +5,9 @@ function [ combinedMAT ] = combineMAT( matFileList )
 combinedMAT = {};
 
 for P=1:length(matFileList)
+    if 0 == mod(P,10000)
+        disp(P)
+    end
     if strcmp(matFileList{P,2},'skipped') | strcmp(matFileList{P,2},'error')
         continue;
     end
