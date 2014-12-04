ef052feb1f80e53ef15af0de85fb74b72800830c
syedshabihhasan <hasanshabih@gmail.com>
2014-12-04 11:06:22 -0600

    functions saves variables in the middle to save on memory

--- Code/UtilityCode/groundTruthCollection/createFlatDataset.m
+++ Code/UtilityCode/groundTruthCollection/createFlatDataset.m
@@ -1,7 +1,10 @@
-function [ flatFile ] = createFlatDataset( actualDataset )
+function [ flatFile ] = createFlatDataset( actualDataset,savePath )
 %CREATEFLATDATASET convert double based dataset to a single binary string
 %   @author: Syed Shabih Hasan
 
+if 1 == nargin
+    savePath = '.';
+end
 flatFile = [];
 [r,~] = size(actualDataset);
 
@@ -16,6 +19,10 @@ for P=1:r
         temp = strcat(temp,doub2bin(toConsider(Q)));
     end
     flatFile = [flatFile; temp;];
+    if 0 == mod(P,100000)
+        save(sprintf('%s/final_flat_%d',P),'flatFile');
+        flatFile = [];
+    end
 end
 
 end
