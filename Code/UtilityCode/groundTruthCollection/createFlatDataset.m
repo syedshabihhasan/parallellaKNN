752e38a67efc8e758a794fea60263597c602275d
syedshabihhasan <hasanshabih@gmail.com>
2014-12-04 10:54:27 -0600

    functions to create binary strings of our dataset

---
+++ Code/UtilityCode/groundTruthCollection/createFlatDataset.m
@@ -0,0 +1,20 @@
+function [ flatFile ] = createFlatDataset( actualDataset )
+%CREATEFLATDATASET convert double based dataset to a single binary string
+%   @author: Syed Shabih Hasan
+
+flatFile = [];
+[r,~] = size(actualDataset);
+
+for P=1:r
+    toConsider = actualDataset(P,2:end);
+    temp = '';
+    [~,n] = size(toConsider);
+    for Q=1:n
+        temp = strcat(temp,doub2bin(toConsider(Q)));
+    end
+    flatFile = [flatFile; temp;];
+    temp = [];
+end
+
+end
+
