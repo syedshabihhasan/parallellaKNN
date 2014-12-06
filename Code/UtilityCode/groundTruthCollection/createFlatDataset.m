654b2857bddde3325f6a763d0fd4a3ce2b08e01f
syedshabihhasan <hasanshabih@gmail.com>
2014-12-06 09:15:04 -0600

    uncommented the wrong portion

--- Code/UtilityCode/groundTruthCollection/createFlatDataset.m
+++ Code/UtilityCode/groundTruthCollection/createFlatDataset.m
@@ -31,16 +31,16 @@ delete(parObj);
 save(sprintf('%s/cellFlat',savePath),'flatFileCell','-v7.3');
 disp('Done with parallel job!');
 flatFile = cell2mat(flatFileCell);
-save(sprintf('%s/final_flat',savePath),'flatFile');
-for P=1:r
-    if 0 == mod(P,10000)
-        disp(sprintf('I am at P=%d',P));
-    end
-    flatFile = [flatFile; flatFileCell{P}];
-    if 0 == mod(P,100000)
-       save(sprintf('%s/final_flat_%d',P),'flatFile','-v7.3');
-       flatFile = [];
-   end
-end
+save(sprintf('%s/final_flat',savePath),'flatFile','-v7.3');
+% for P=1:r
+%     if 0 == mod(P,10000)
+%         disp(sprintf('I am at P=%d',P));
+%     end
+%     flatFile = [flatFile; flatFileCell{P}];
+%     if 0 == mod(P,100000)
+%        save(sprintf('%s/final_flat_%d',P),'flatFile','-v7.3');
+%        flatFile = [];
+%    end
+% end
 end
 
