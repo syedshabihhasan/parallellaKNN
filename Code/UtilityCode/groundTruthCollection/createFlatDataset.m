5f09811ab93c06732c2f4827f412dfa1348bac77
syedshabihhasan <hasanshabih@gmail.com>
2014-12-06 09:11:53 -0600

    large files can be stored with the -v7.3 flag, switching to parallel mode

--- Code/UtilityCode/groundTruthCollection/createFlatDataset.m
+++ Code/UtilityCode/groundTruthCollection/createFlatDataset.m
@@ -5,42 +5,42 @@ function [ flatFile ] = createFlatDataset( actualDataset,savePath )
 if 1 == nargin
     savePath = '.';
 end
-% flatFile = '';
+flatFile = '';
 [r,~] = size(actualDataset);
-% flatFileCell = cell(r,1);
-%parObj = parpool;
+flatFileCell = cell(r,1);
+parObj = parpool;
 toSave = strcat(savePath,'/opBin.txt');
-f = fopen(toSave,'w');
-for P=1:r
-    if 0 == mod(P,10000)
-        disp(sprintf('P=%d',P));
-    end
-    %disp(sprintf('Parfor, P=%d',P));
+% f = fopen(toSave,'w');
+parfor P=1:r
+    %if 0 == mod(P,10000)
+    %    disp(sprintf('P=%d',P));
+    %end
+    disp(sprintf('Parfor, P=%d',P));
     toConsider = actualDataset(P,2:end);
     temp = '';
     [~,n] = size(toConsider);
     for Q=1:n
         temp = strcat(temp,doub2bin(toConsider(Q)));
     end
-    fprintf(f,'%s\n',temp);
-%     flatFileCell{P} = temp;
+%     fprintf(f,'%s\n',temp);
+    flatFileCell{P} = temp;
 
 end
-fclose(f);
-%delete(parObj);
-% save(sprintf('%s/cellFlat',savePath),'flatFileCell');
-% disp('Done with parallel job!');
-% flatFile = cell2mat(flatFileCell);
-% save(sprintf('%s/final_flat',savePath),'flatFile');
-% for P=1:r
-%     if 0 == mod(P,10000)
-%         disp(sprintf('I am at P=%d',P));
-%     end
-%     flatFile = [flatFile; flatFileCell{P}];
-%     if 0 == mod(P,100000)
-%        save(sprintf('%s/final_flat_%d',P),'flatFile');
-%        flatFile = [];
-%    end
-% end
+% fclose(f);
+delete(parObj);
+save(sprintf('%s/cellFlat',savePath),'flatFileCell','-v7.3');
+disp('Done with parallel job!');
+flatFile = cell2mat(flatFileCell);
+save(sprintf('%s/final_flat',savePath),'flatFile');
+for P=1:r
+    if 0 == mod(P,10000)
+        disp(sprintf('I am at P=%d',P));
+    end
+    flatFile = [flatFile; flatFileCell{P}];
+    if 0 == mod(P,100000)
+       save(sprintf('%s/final_flat_%d',P),'flatFile','-v7.3');
+       flatFile = [];
+   end
+end
 end
 
