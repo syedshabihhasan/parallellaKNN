2d04589f806482986ebcc568f15dd77cbd8a6000
vivek-sardeshmukh@uiowa.edu <vivek-sardeshmukh@uiowa.edu>
2014-12-07 17:45:03 -0600

    hash function - needs review

--- Code/Infrastructure/HashFunctions.c
+++ Code/Infrastructure/HashFunctions.c
@@ -11,24 +11,32 @@
 
 
 unsigned int getRandomIndex(int min, int max){
-    return min+int((max-min+1)*rand()/(RAND_MAX+1.0));
+    return min + (int)((max-min+1)*rand()/(RAND_MAX+1.0));
 }
 
 void HashFunctionsInit(unsigned int seed){
     unsigned short int i, j;
     srand(seed);
-    for(i = 0; i < MAX_HASH_FUNC; i++){
-	for(j = 0; j < HASH_FUNC_WIDTH; j++){
-	    HASH_FUNC[i][j] = getRandomIndex(0, RECORD_SIZE);
+    mask[0] = 1;
+    for(i = 1; i < RECORD_SIZE; i++)
+	mask[i] = 2*mask[i-1]; 
+    for(i = 0; i < MAX_HASH_FUNCTION; i++){
+	for(j = 0; j < HASH_FUNCTION_WIDTH; j++){
+	    HASH_FUNC[i][j] = getRandomIndex(32, RECORD_SIZE);
 	}
     }
 }
 
 unsigned int HashValue(void *record, unsigned int functionNumber){
-    unsigned int hashvalue;
-
-
-    return hashvalue;
+    unsigned int hashValue;
+    unsigned char maskThis;
+    int i;
+    maskThis = 0;
+    for(i = 0; i < HASH_FUNCTION_WIDTH; i++){
+	maskThis = maskThis + mask[HASH_FUNC[functionNumber][i]];
+    }
+    hashValue = (unsigned int)(*(unsigned int*)record & maskThis);
+    return hashValue;
 }
 
 
--- Code/Infrastructure/HashFunctions.h
+++ Code/Infrastructure/HashFunctions.h
@@ -4,12 +4,16 @@
 #ifndef HASHFUNCTIONS_H
 #define HASHFUNCTIONS_H
 
+/*Length of a record in bits*/
+#define RECORD_SIZE 8192
 /*Number of hash functions*/
 #define MAX_HASH_FUNCTION 32
 /*Widht of a hash function - these many random index are used*/
 #define HASH_FUNCTION_WIDTH 24
 /*Array holding random index for each hash function*/
-unsigned int HASH_FUNC[MAX_HASH_FUNC][HASH_FUNC_WIDTH];
+unsigned int HASH_FUNC[MAX_HASH_FUNCTION][HASH_FUNCTION_WIDTH];
+/*mast bits*/
+static unsigned char mask[RECORD_SIZE];
 
 /*init function needs to be called at the begining*/
 void HashFunctionsInit(unsigned int seed);
