64d6125ffa14919513de3a90e66f73c111a340bf
syedshabihhasan <hasanshabih@gmail.com>
2014-12-08 16:30:30 -0600

    KNN search in parts

--- Code/UtilityCode/groundTruthCollection/doParKNN.m
+++ Code/UtilityCode/groundTruthCollection/doParKNN.m
@@ -1,14 +1,26 @@
-function [ knnres ] = doParKNN( flatFilePath, K )
+function [ knnres ] = doParKNN( flatFilePath, K, N )
 %DOPARKNN Summary of this function goes here
 %   @author: Syed Shabih Hasan
 
-load(flatFilePath);
-[r,~] = size(flatFile);
+t = load(flatFilePath);
+flatFile = t.flatFile;
+clear t;
+if 1 == N
+    tempFlatFile = flatFile(1:250000,:);
+elseif 2 == N
+    tempFlatFile = flatFile(251000:500000,:);
+elseif 3 == N
+    tempFlatFile = flatFile(500001:750000,:);
+elseif 4 == N
+    tempFlatFile = flatFile(750000:end,:);
+end
+[r,~] = size(tempFlatFile);
 parObj = parpool;
 knnres = zeros(r,K);
 parfor P=1:r
     disp(sprintf('LoopPos=%d',P));
-    knnres(P,:) = knnsearch(flatFile,flatFile(P,:),'K',K,'Distance','hamming');
+    temp = tempFlatFile(P,:);
+    knnres(P,:) = knnsearch(flatFile,temp,'K',K,'Distance','hamming');
 end
 delete(parObj);
 
