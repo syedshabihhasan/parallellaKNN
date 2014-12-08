d07f8189968f4ba83bc27ea288b4cf285ed0af6f
syedshabihhasan <hasanshabih@gmail.com>
2014-12-08 09:31:07 -0600

    there were a few errors, use try catch

--- Code/UtilityCode/convertToBinary/redoIndexing.m
+++ Code/UtilityCode/convertToBinary/redoIndexing.m
@@ -4,10 +4,19 @@ function redoIndexing( binFilePath )
 
 P=1;
 f = fopen(binFilePath,'r+b');
-
-for P=1:997747
-    fwrite(f,P,'int',0,'l');
-    fseek(f,1020,0);
+if -1 == f
+    disp('fid = -1, existing');
+    exit;
+end
+while ~feof(f)
+    try
+        fwrite(f,P,'int',0,'l');
+        fseek(f,1020,0);
+        P = P+1;
+    catch err
+        disp(sprintf('An error occured at P = %d',P));
+        err
+    end
 end
 
 fclose(f);
