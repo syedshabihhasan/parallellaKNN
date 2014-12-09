b158c153870ae29ee1a9da4170ae0d7d61dccc4c
syedshabihhasan <hasanshabih@gmail.com>
2014-12-09 09:59:15 -0600

    header for doing ground truth search

---
+++ Code/UtilityCode/groundTruthKNNInC/src/knnHeader.h
@@ -0,0 +1,22 @@
+/*
+ * knnHeader.h
+ *
+ *      Author: Syed Shabih Hasan
+ */
+
+#ifndef KNNHEADER_H_
+#define KNNHEADER_H_
+
+#define MAX_RECORD 		997747
+#define RECORD_LEN		256
+#define	ZERO			0x00000000
+#define	ONE				0x00000001
+#define TWO				0x00000002
+#define FOUR			0x00000004
+#define	EIGHT			0x00000008
+#define SIXTEEN 		0x00000010
+#define THIRTY_TWO		0x00000020
+#define K				10
+
+
+#endif /* KNNHEADER_H_ */
